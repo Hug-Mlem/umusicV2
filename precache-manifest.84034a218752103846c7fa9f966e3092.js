07661d2066f77ab250e3f67b08b22fed3baaba49
@ringme_hunglm <hunglm@ringme.vn>
2022-03-04 10:09:58 +0700

    Updates

---
+++ precache-manifest.84034a218752103846c7fa9f966e3092.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4958aa37dcc76ee5ebe9dc239571f8",
+    "url": "/umusicV2/index.html"
+  },
+  {
+    "revision": "bcd72f540bbd2911d194",
+    "url": "/umusicV2/static/css/1.d2744ea4.chunk.css"
+  },
+  {
+    "revision": "e8d39d5b9d1e58d003b6",
+    "url": "/umusicV2/static/css/10.729abcda.chunk.css"
+  },
+  {
+    "revision": "65d1636d9a2ea102d71f",
+    "url": "/umusicV2/static/css/13.fe17e490.chunk.css"
+  },
+  {
+    "revision": "6119c9818974d44be378",
+    "url": "/umusicV2/static/css/17.87a9b032.chunk.css"
+  },
+  {
+    "revision": "695b43fbe81247772f58",
+    "url": "/umusicV2/static/css/18.87a9b032.chunk.css"
+  },
+  {
+    "revision": "df39be3af44a1b24cb01",
+    "url": "/umusicV2/static/css/19.87a9b032.chunk.css"
+  },
+  {
+    "revision": "9fceee68d6d9d2437166",
+    "url": "/umusicV2/static/css/2.729abcda.chunk.css"
+  },
+  {
+    "revision": "124f5486c64309c2bc69",
+    "url": "/umusicV2/static/css/20.570d36b0.chunk.css"
+  },
+  {
+    "revision": "8bb4526ec53028c70811",
+    "url": "/umusicV2/static/css/7.729abcda.chunk.css"
+  },
+  {
+    "revision": "d9240565de1dcf774b71",
+    "url": "/umusicV2/static/css/8.2a6aab7a.chunk.css"
+  },
+  {
+    "revision": "337d01fee4f87b6e4731",
+    "url": "/umusicV2/static/css/9.729abcda.chunk.css"
+  },
+  {
+    "revision": "b7b7e86f2574a21fe9bc",
+    "url": "/umusicV2/static/css/main.fb259099.chunk.css"
+  },
+  {
+    "revision": "0f0a5fec726aeae4e65b",
+    "url": "/umusicV2/static/js/0.f16801fc.chunk.js"
+  },
+  {
+    "revision": "bcd72f540bbd2911d194",
+    "url": "/umusicV2/static/js/1.e568f437.chunk.js"
+  },
+  {
+    "revision": "f939262c408b915460528c647d7afcc0",
+    "url": "/umusicV2/static/js/1.e568f437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d39d5b9d1e58d003b6",
+    "url": "/umusicV2/static/js/10.48990f72.chunk.js"
+  },
+  {
+    "revision": "71fa4a4d6475e20a011a",
+    "url": "/umusicV2/static/js/11.8d40a61e.chunk.js"
+  },
+  {
+    "revision": "f80bccd108dac411258b",
+    "url": "/umusicV2/static/js/12.68460ceb.chunk.js"
+  },
+  {
+    "revision": "65d1636d9a2ea102d71f",
+    "url": "/umusicV2/static/js/13.c23e4e54.chunk.js"
+  },
+  {
+    "revision": "7b57bc95cb3816fdde31",
+    "url": "/umusicV2/static/js/14.b25e377a.chunk.js"
+  },
+  {
+    "revision": "0b3eb635fd9f02bbf687",
+    "url": "/umusicV2/static/js/15.560f1852.chunk.js"
+  },
+  {
+    "revision": "112acd4bb7dfda14b536",
+    "url": "/umusicV2/static/js/16.92b24fb6.chunk.js"
+  },
+  {
+    "revision": "6119c9818974d44be378",
+    "url": "/umusicV2/static/js/17.4f18f445.chunk.js"
+  },
+  {
+    "revision": "695b43fbe81247772f58",
+    "url": "/umusicV2/static/js/18.ee79a6b3.chunk.js"
+  },
+  {
+    "revision": "df39be3af44a1b24cb01",
+    "url": "/umusicV2/static/js/19.f9fc49bd.chunk.js"
+  },
+  {
+    "revision": "9fceee68d6d9d2437166",
+    "url": "/umusicV2/static/js/2.3256b963.chunk.js"
+  },
+  {
+    "revision": "124f5486c64309c2bc69",
+    "url": "/umusicV2/static/js/20.00e07df8.chunk.js"
+  },
+  {
+    "revision": "11edb8d51c2f3cfe1b25",
+    "url": "/umusicV2/static/js/21.be5525f7.chunk.js"
+  },
+  {
+    "revision": "aaed3bf5fc4407030c3f",
+    "url": "/umusicV2/static/js/22.8b613957.chunk.js"
+  },
+  {
+    "revision": "e52231f26df1e7c0308a",
+    "url": "/umusicV2/static/js/23.a01fcc8e.chunk.js"
+  },
+  {
+    "revision": "64fde6621acbae6411b8",
+    "url": "/umusicV2/static/js/24.b7e08de3.chunk.js"
+  },
+  {
+    "revision": "e9743b3c072309cc5a06",
+    "url": "/umusicV2/static/js/5.50ec884f.chunk.js"
+  },
+  {
+    "revision": "02bf04c7217700b266d5ef477dd76f6e",
+    "url": "/umusicV2/static/js/5.50ec884f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f73c9666eae80d6cc4",
+    "url": "/umusicV2/static/js/6.cc83bfb7.chunk.js"
+  },
+  {
+    "revision": "8bb4526ec53028c70811",
+    "url": "/umusicV2/static/js/7.5591c576.chunk.js"
+  },
+  {
+    "revision": "d9240565de1dcf774b71",
+    "url": "/umusicV2/static/js/8.ffee3ca3.chunk.js"
+  },
+  {
+    "revision": "337d01fee4f87b6e4731",
+    "url": "/umusicV2/static/js/9.b3ecc1c6.chunk.js"
+  },
+  {
+    "revision": "b7b7e86f2574a21fe9bc",
+    "url": "/umusicV2/static/js/main.d24de300.chunk.js"
+  },
+  {
+    "revision": "e4edbaa2f4980208a905",
+    "url": "/umusicV2/static/js/runtime-main.a4813550.js"
+  },
+  {
+    "revision": "71d1b3a59a3bd9205355b69665233156",
+    "url": "/umusicV2/static/media/plus.71d1b3a5.svg"
+  },
+  {
+    "revision": "88fd28590406755213d5dd23e435c5c7",
+    "url": "/umusicV2/static/media/vip.88fd2859.png"
+  }
+]);
